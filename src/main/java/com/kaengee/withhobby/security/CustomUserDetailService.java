9b8f9c77c280d2adb4f1569e81e8abd9be2894fc
KaengEE <yopuiqwer@naver.com>
2024-01-09 15:28:00 +0900

    userId => username

--- src/main/java/com/kaengee/withhobby/security/CustomUserDetailService.java
+++ src/main/java/com/kaengee/withhobby/security/CustomUserDetailService.java
@@ -18,10 +18,10 @@ public class CustomUserDetailService implements UserDetailsService {
     private final UserService userService;
 
     @Override
-    public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         //DB에서 유저 검색
-        User user = userService.findByUserId(userId).orElseThrow(()->
-                new UsernameNotFoundException(userId));
+        User user = userService.findByUsername(username).orElseThrow(()->
+                new UsernameNotFoundException(username));
 
         //user의 role을 스프링시큐리티의 GrantedAuthority로 바꿔준다. 여러개의 role을 가질수 있으므로 Set
         Set<GrantedAuthority> authorities = Set.of(SecurityUtils.convertToAuthority(user.getRole().name()));
