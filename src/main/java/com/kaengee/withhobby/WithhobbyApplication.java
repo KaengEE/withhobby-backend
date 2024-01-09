b919a62b7c9a5516415e67b74950592a5284fa60
KaengEE <yopuiqwer@naver.com>
2024-01-09 09:25:55 +0900

    프로젝트 시작

---
+++ src/main/java/com/kaengee/withhobby/WithhobbyApplication.java
@@ -0,0 +1,13 @@
+package com.kaengee.withhobby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithhobbyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithhobbyApplication.class, args);
+	}
+
+}
