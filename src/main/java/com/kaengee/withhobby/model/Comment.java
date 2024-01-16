3739fce797ecfbe134880fb25913359c650d5545
KaengEE <yopuiqwer@naver.com>
2024-01-16 10:10:56 +0900

    엔터티수정

--- src/main/java/com/kaengee/withhobby/model/Comment.java
+++ src/main/java/com/kaengee/withhobby/model/Comment.java
@@ -15,24 +15,18 @@ public class Comment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    //한개의글에 여러개 댓글
+    // 한 개의 글에 여러 개 댓글
     @ManyToOne(fetch = FetchType.EAGER)
     private Post post;
 
-    //post테이블의 user_id 참조 = 글쓴이
+    // 한 명이 여러 개 댓글 작성
     @ManyToOne(fetch = FetchType.EAGER)
-    private Post writer;
-
-    //한명이 여러개 댓글 작성
-    @ManyToOne(fetch = FetchType.EAGER)
-    private User user; //댓글작성자
+    private User user; // 댓글 작성자
 
     @Column(name = "comment_text", nullable = false, length = 500)
     private String commentText; //댓글내용
 
     @Column(name = "create_at", nullable = false)
     private LocalDateTime createAt;
-
-
 
 }
